6cb958d28b7ecdb28f452178d8d054eddc6cd18f
Oscar Lai <tlai4178@uni.sydney.edu.au>
2018-06-11 23:12:25 +1000

    mouse sampler now continuously updates as mouse move

--- mouseSampler.py
+++ mouseSampler.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import MOUSEBUTTONDOWN
+import time
 from baseSampler import Sampler
 """
 For demo / testing only. This policy wait for user mouse input for next sampling node.
@@ -8,17 +8,16 @@ For demo / testing only. This policy wait for user mouse input for next sampling
 class MouseSampler(Sampler):
 
     def init(self, **kwargs):
-        self.XDIM = kwargs['XDIM']
-        self.YDIM = kwargs['YDIM']
-        self.RRT = kwargs['RRT']
         self.SCALING = kwargs['SCALING']
 
+    def getMouseClickPosition(self):
+        while True:
+            time.sleep(0.05)
+            pygame.event.wait()
+            if pygame.mouse.get_pressed()[0] == 1: # Left mouse pressed
+                pos = pygame.mouse.get_pos()
+                pos = (int(pos[0] / self.SCALING), int(pos[1] / self.SCALING))
+                return pos
+
     def getNextNode(self):
-        next_node = None
-        while next_node is None:
-            for e in pygame.event.get():
-                if e.type == MOUSEBUTTONDOWN:
-                    mousePos = (int(e.pos[0] / self.SCALING), int(e.pos[1] / self.SCALING))
-                    if next_node is None:
-                        next_node = mousePos
-        return next_node, self.reportSuccess, self.reportFail
+        return self.getMouseClickPosition(), self.reportSuccess, self.reportFail
