a6f2fe858774512a877be1ff63430403b947718e
Oscar Lai <tlai4178@uni.sydney.edu.au>
2018-06-29 00:05:43 +1000

    refactored script into helpers

--- benchmark_combine_stats.py
+++ benchmark_combine_stats.py
@@ -1,19 +1,19 @@
-import sys, os, glob
+import sys, os
 import re
-import csv
 
-import rrtstar
-import subprocess
 import openpyxl
 from openpyxl import Workbook
 from openpyxl.chart import (
-    ScatterChart,
     Reference,
-    Series,
 )
-from openpyxl.chart.error_bar import ErrorBars
-from openpyxl.chart import data_source
-from openpyxl.utils import get_column_letter
+from openpyxl_helper import (
+    duplicate_col,
+    save_csv_as_sheet,
+    RefFormula,
+    split_to_append_text,
+    split_to_funcs,
+    build_scatter_with_mean_stdev
+    )
 
 STATS_FILE_NAME = 'stats.xlsx'
 
@@ -25,40 +25,8 @@ DATA_COST_COL = 6  # column number of cost
 DATA_MAX_COL = 6  # amount of column for data section
 DATA_NUMNODE_COL = 1  # column number of number of nodes
 
-def duplicate_col(start, end, ws, row, formatted_str, datas_rotates):
-    # convert to 0-based system
-    start -= 1
-    end -= 1 # end is assume to not be inclusive
-    _count = 0
-    for column in range(start, (end-start)*2+start):
-        column += 1  # convert to 1-based system
-        column_letter = get_column_letter(int((1+column-start)/2) + start)
-        # switch between two types on each iterations
-        data = datas_rotates[_count]
-        _count += 1
-        if _count >= len(datas_rotates):
-            _count = 0
-        val = formatted_str.format(c=column_letter, r=row, data=data)
-        ws.cell(row=row, column=column, value=val)
 
 def combine_result(folder):
-
-    def save_csv_as_sheet(policy):
-        # create a list to store the newly created sheets
-        sheets = []
-        for filename in glob.glob("policy={}_*.csv".format(policy)):
-            timestamp = re.search('.*timestamp=([0-9]+-[0-9]+).csv', filename).group(1)
-            newsheet = "{}_{}".format(policy, timestamp)
-            ws = wb.create_sheet(newsheet)
-            sheets.append(newsheet)
-            with open(filename) as f:
-                reader = csv.reader(f)
-                for row_index, row in enumerate(reader):
-                    row_index += 1 # conver to 1-based system
-                    if row_index > DATA_BEGIN_ROW:
-                        row = (float(x) if x != 'inf' else x for x in row)
-                    ws.append(row)
-        return sheets, ws.max_row
 
     def create_main_stats():
         ############################################################
@@ -69,40 +37,37 @@ def combine_result(folder):
         for i in range(start, end):
             plotting_col = (i-start)*2 + start
 
-            chart = ScatterChart()
-            chart.title = wb[sheets[0]].cell(row=DATA_BEGIN_ROW, column=i).value
-            # chart.style = 13
-            chart.x_axis.title = "Number of Nodes"
-            chart.y_axis.title = wb[sheets[0]].cell(row=DATA_BEGIN_ROW, column=i).value
             xvalues = Reference(wb[policies[0]], min_col=1, min_row=DATA_BEGIN_ROW, max_row=wb[policies[0]].max_row)
+            yvalues = []
+            stdevs = []
+            titles = []
             for j, policy in enumerate(policies):
                 stat_ws = wb[policy]
-                values = Reference(stat_ws, min_col=plotting_col, min_row=DATA_BEGIN_ROW, max_row=stat_ws.max_row)
-                stdev = Reference(stat_ws, min_col=plotting_col+1, min_row=DATA_BEGIN_ROW+1, max_row=stat_ws.max_row)
-
-                # convert reference to data source
-                stdev_data_source = data_source.NumDataSource(numRef=data_source.NumRef(f=stdev))
-                error_bars = ErrorBars(errDir='y', errValType='cust', plus=stdev_data_source, minus=stdev_data_source)
-
-                series = Series(values, xvalues, title=policy)
-                series.errBars = error_bars
-                chart.series.append(series)
+                yvalues.append(Reference(stat_ws, min_col=plotting_col, min_row=DATA_BEGIN_ROW, max_row=stat_ws.max_row))
+                stdevs.append(Reference(stat_ws, min_col=plotting_col+1, min_row=DATA_BEGIN_ROW+1, max_row=stat_ws.max_row))
+                titles.append(policy)
                 ############################################################
                 ##   Store simple stats at main screen for easy viewing   ##
                 ############################################################
                 ws.cell(row=2+j, column=1, value=policy)
                 for local_col_idx, col in enumerate(range(INFO_MAX_COL + 1, INFO_MAX_COL + INFO_EXTRA_COL*2 + 1)):
                     # Set titles
-                    val = "='{sheet}'!{c}{r}".format(sheet=policy,
-                                                    c=get_column_letter(col),
-                                                    r=1)
+                    val = RefFormula(worksheet=policy,
+                                    min_col=col, min_row=1)
                     ws.cell(row=1, column=local_col_idx+2, value=val)
                     # Set values
-                    val = "='{sheet}'!{c}{r}".format(sheet=policy,
-                                                    c=get_column_letter(col),
-                                                    r=2)
+                    val = RefFormula(worksheet=policy,
+                                    min_col=col, min_row=2)
                     ws.cell(row=2+j, column=local_col_idx+2, value=val)
 
+            chart = build_scatter_with_mean_stdev(xvalues=xvalues,
+                                                  yvalues_refs=yvalues,
+                                                  stdev_refs=stdevs,
+                                                  titles=titles)
+            chart.title = wb[sheets[0]].cell(row=DATA_BEGIN_ROW, column=i).value
+            # chart.style = 13
+            chart.x_axis.title = "Number of Nodes"
+            chart.y_axis.title = wb[sheets[0]].cell(row=DATA_BEGIN_ROW, column=i).value
             ws.add_chart(chart, 'A'+str(4+15*(i-start)))
 
 
@@ -119,7 +84,16 @@ def combine_result(folder):
 
     policies = ['random', 'disjoint']
     for idx, policy in enumerate(policies):
-        sheets, num_rows = save_csv_as_sheet(policy)
+
+        sheets, num_rows = save_csv_as_sheet(
+            wb=wb,
+            filename_glob="policy={}_*.csv".format(policy),
+            get_sheetname_func=lambda fn, policy=policy : "{}_{}".format(policy,
+                                re.search('.*timestamp=([0-9]+-[0-9]+).csv', fn).group(1)),
+            row_filter=lambda row_idx, row, z=DATA_BEGIN_ROW :
+                        row if row_idx <= z
+                        else (float(x) if x != 'inf' else x for x in row))
+
         ws = wb.create_sheet(policy, index=idx)
         create_stats_view(ws, sheets, num_rows=num_rows)
         create_stats_time_taken(wb, raw_sheets=sheets, stat_ws=ws)
@@ -142,6 +116,7 @@ def combine_all_results(maindir):
     for subdir in all_dirs:
         combine_result(subdir)
         os.chdir(maindir) # switch back to main folder
+        return
 
 ############################################################
 ##                    HELPER FUNCTIONS                    ##
@@ -185,27 +160,29 @@ def create_stats_time_taken(wb, raw_sheets, stat_ws):
                   end=INFO_MAX_COL+5,
                   ws=stat_ws,
                   row=1,
-                  formatted_str="='{sh}'!{{c}}{{r}}&\"{{data}}\"".format(sh=raw_sheets[0]),
-                  datas_rotates=('(mean)', '(stdev)'))
+                  name_generator=lambda sh=raw_sheets[-1],
+                                      data=('(mean)', '(stdev)')
+                                      : split_to_append_text(sh, data),
+                  multiples=2)
     duplicate_col(start=INFO_MAX_COL+1,
                   end=INFO_MAX_COL+5,
                   ws=stat_ws,
                   row=2,
-                  formatted_str="={{data}}('{s_begin}:{s_end}'!{{c}}{{r}})".format(
-                      s_begin=raw_sheets[0],
-                      s_end=raw_sheets[-1]),
-                  datas_rotates=('AVERAGE','STDEV'))
+                  name_generator=lambda sheet_begin=raw_sheets[0],
+                                      sheet_end=raw_sheets[-1],
+                                      data=('AVERAGE','STDEV')
+                                      : split_to_funcs(sheet_begin, sheet_end, data),
+                  multiples=2)
 
 def create_stats_view(main_ws, sheets, num_rows):
     ############################################################
     ##             first 4 rows just mimic others             ##
     ############################################################
     for column in range(1, INFO_MAX_COL + 1):
-        column_letter = get_column_letter(column)
         for row in range(1, INFO_END_ROW + 1):
-            val = "='{sheet}'!{c}{r}".format(sheet=sheets[0],
-                                            c=column_letter,
-                                            r=row)
+            val = RefFormula(worksheet=sheets[0],
+                            min_col=column, min_row=row)
+
             main_ws.cell(row=row, column=column, value=val)
             # main_ws[column_letter + str(row)] =
     ############################################################
@@ -214,8 +191,9 @@ def create_stats_view(main_ws, sheets, num_rows):
     ## Title row
     row = 4
     # First title in column A:
-    val = "='{sheet}'!{c}{r}".format(sheet=sheets[0],
-                                    c='A',r=row)
+    val = RefFormula(worksheet=sheets[0],
+                    min_col=1, min_row=row)
+
     main_ws.cell(row=row, column=1, value=val)
     # others
 
@@ -223,8 +201,10 @@ def create_stats_view(main_ws, sheets, num_rows):
                   end=DATA_MAX_COL+1,
                   ws=main_ws,
                   row=row,
-                  formatted_str="='{sh}'!{{c}}{{r}}&\"{{data}}\"".format(sh=sheets[0]),
-                  datas_rotates=('(mean)', '(stdev)'))
+                  name_generator=lambda sh=sheets[0],
+                                      data=('(mean)', '(stdev)')
+                                      : split_to_append_text(sh, data),
+                  multiples=2)
     ############################################################
     ## Values
     for row in range(DATA_BEGIN_ROW + 1, num_rows + 1):
@@ -236,10 +216,11 @@ def create_stats_view(main_ws, sheets, num_rows):
                       end=DATA_MAX_COL+1,
                       ws=main_ws,
                       row=row,
-                      formatted_str="={{data}}('{s_begin}:{s_end}'!{{c}}{{r}})".format(
-                          s_begin=sheets[0],
-                          s_end=sheets[-1]),
-                      datas_rotates=('AVERAGE','STDEV'))
+                      name_generator=lambda sheet_begin=sheets[0],
+                                          sheet_end=sheets[-1],
+                                          data=('AVERAGE','STDEV')
+                                          : split_to_funcs(sheet_begin, sheet_end, data),
+                      multiples=2)
 
 if __name__ == '__main__':
     dirname = os.path.abspath('benchmark_copy')
